f0ca0089b9eb53625e9cb83eba596bda88950875
ruhi16 <hndas16@gmail.com>
2017-04-08 19:17:06 +0530

    new change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel, Hari Naryan Das.. :)
+                    Welcome, Hari Naryan Das
                 </div>
 
                 <div class="links">
