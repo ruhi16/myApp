6b0ee406e7539e6d28e4fae54f6048841cbdd59b
ruhi16 <hndas16@gmail.com>
2017-04-10 07:14:59 +0530

    web.php modified

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
         echo "Email: ".$u->email."<br>";
         echo "Password: ".$u->password."<br><br>";
     }
+    echo "Change effected";
     //return "Date Saved Successfully for ayantika";
     //return view('welcome');
 });
